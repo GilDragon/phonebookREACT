e247cd9934baf5dab114be0400d9bba6d2732214
GilDragon <PAR13371158@student.kangan.edu.au>
2023-01-17 12:21:33 +1100

    only number

--- src/components/Add-person.js
+++ src/components/Add-person.js
@@ -6,12 +6,12 @@ class Addperson extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            addcontact: { addName: "Enter any name", addNumber: "Enter any number"}
+            Addperson: null
         }
     }
 
     addperson() {
-    
+        
         const n = document.getElementById("add-addName-inp").value;
         const an = document.getElementById("add-addNumber-inp").value;
         //const ln = document.getElementById("add-lastname-inp").value;
@@ -20,14 +20,19 @@ class Addperson extends React.Component {
         //body: JSON.stringify({ title: 'react POST request example' })
 
         const newContact = { name: n, contactNumber: an}
-
+        if(n === "" || an === "")
+        return;
         fetch("https://kanganphonebookapi.azurewebsites.net/contact?",
             {
                 headers: { "Content-Type": "application/json" },
                 method: 'POST',
                 body: JSON.stringify(newContact)
             })
-            .then(res => console.log(res))
+            .then(r => r.json())
+            .then(res => this.setState({Addperson: res}))
+            .then(console.log(this.state.Addperson))
+            //.then(re => this.setState({n: re}))
+            //.then(console.log(this.state.n))
 
                 
         //+ new URLSearchParams({ addName: n, addNumber: an }))
@@ -44,10 +49,18 @@ class Addperson extends React.Component {
         return (
             <div className="Addperson">
                 <h4>Add person</h4>
-                     <input type="text" id="add-addName-inp" /> <input type="text" id="add-addNumber-inp" />
+                    <b>Name:</b><input type="text" id="add-addName-inp" /><b>Phone number:</b><input type="number" id="add-addNumber-inp" />
                      
                 <button onClick={() => this.addperson()}>Add</button>
                 <div style={{padding: "10px", color: 'purple', fontWeight: 'bold'}}>Enter First name, Last name and Contact number (EX) Gilsoo Park 0421971531</div>
+                    <div>
+                    {
+                        this.state.Addperson === null ?
+                        <></>: this.state.Addperson === null ?
+                            "Name or Phone number have to be filled" : <b>A contact has been added</b>
+
+                    }
+                </div>
             </div>
             
             // if the id-name and id-number goes into database successfully return new contact has been added
